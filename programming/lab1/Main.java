8ea204e29d1c018a8fac031af3b507d2d80ddf40
yargames1 <89339132+yargames1@users.noreply.github.com>
2024-09-07 23:43:03 +1100

    Пункт 2

--- programming/lab1/Main.java
+++ programming/lab1/Main.java
@@ -10,7 +10,13 @@ public class Main {
 		for (int i = 0; i<=17; i++) { // Заполнить его числами от 3 до 20 включительно в порядке убывания
 			f[i] = (int) (20 - i);
 		}
-		System.out.println(Arrays.toString(f));
+		// Пункт 2
+		Random random = new Random();
+		double[] x = new double[18]; // Создать одномерный массив x типа double.
+		for (int i = 0; i<=17; i++){ // Заполнить его 18-ю случайными числами в диапазоне от -4.0 до 12.0.
+			x[i] = (double) random.nextDouble()*16 - 4;
+		}
+		System.out.println(Arrays.toString(x));
 		
 	}
 	
