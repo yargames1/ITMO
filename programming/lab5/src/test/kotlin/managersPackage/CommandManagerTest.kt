666c730ba2c17fcb2b2ba77cba695a8e488c8c37
yargames1 <89339132+yargames1@users.noreply.github.com>
2025-04-10 23:19:13 +0300

    доработка отчета

--- programming/lab5/src/test/kotlin/managersPackage/CommandManagerTest.kt
+++ programming/lab5/src/test/kotlin/managersPackage/CommandManagerTest.kt
@@ -10,15 +10,12 @@ import org.junit.jupiter.api.Assertions.*
 
   @Test
   fun `getCommand should execute the correct command`() {
-   // Мокаем команду и её выполнение
    val mockCommand = mockk<Command>(relaxed = true)
    mockkObject(CommandRegistry)
    every { CommandRegistry.commands } returns mutableMapOf("test" to mockCommand)
 
-   // Вызов getCommand
    CommandManager.getCommand("test arg1 arg2")
 
-   // Проверка, что execute был вызван с правильными аргументами
    verify { mockCommand.execute(listOf("arg1", "arg2")) }
    unmockkAll()
   }
